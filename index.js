dc7945ef4a3daf937e1b02e15fe6b345f1a77e6d
Youssef Mokhtar <youssef96mokhtar@gmail.com>
2023-01-07 05:19:43 +0200

    branch-one index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("Heey you!");
 console.log("A second console");
 console.log("This is a third console!!");
-console.log("Fouurth!!");
+console.log("Fouurth!!")
 console.log("Fifth edited from github");
+console.log("This is branch-one");
+
